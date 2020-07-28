d345517976f32d833634ede680da1456903b314e
Oliwia <oliwia.wojtaszczyk@gmail.com>
2020-07-29 00:39:37 +0200

    checking

--- businesscard.py
+++ businesscard.py
@@ -1,33 +1,59 @@
 from faker import Faker
 
-
-class Card:
-    def __init__(self, first_name, last_name, position, email):
+class BaseContact:
+    def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.position = position
+        self.phone_number = phone_number
         self.email = email
 
     def contact(self):
-        return f"Kontaktuje się z {self.first_name} {self.last_name} - {self.position} email: {self.email}"
+        return f"Wybieram numer {self.phone_number} i dzwonie do {self.first_name} {self.last_name}"
 
-    @property
-    def name_length(self):
-        return len(f"{self.first_name} {self.last_name}")
+class Businesscontact:
+    def __init__(self, position, company, phone_number2):
+        self.position = position
+        self.company = company
+        self.phone_number2 = phone_number2
+
+    def contact(self):
+        return f"Wybieram numer {self.phone_number2} i dzwonie do {self.first_name} {self.last_name}"
 
 
 if __name__ == "__main__":
     faker = Faker("pl_PL")
 
-    for _ in range(10):
-        card = Card(
+def create_contacts(self, BaseContact, Businesscontact):
+    for _ in range(7):
+        create_contacts=create_contacts(
             first_name=faker.first_name(),
             last_name=faker.last_name(),
-            position=faker.job(),
-            email=faker.email()
+            position=faker.position(),
+            company=company.faker()
+             email=faker.email(),
         )
-        print(card.contact())
-        print(card.name_length)
+create_contacts()
+
+    print(BaseContact)
+    or
+    print(Businesscontact)
+    pass
+    print(create_contacts.name_length)
+
+
+
+
+
+
+  #  for _ in range(10):
+   #     card = Card(
+    #        first_name=faker.first_name(),
+     #       last_name=faker.last_name(),
+      #      position=faker.job(),
+       #     email=faker.email()
+        #)
+        #print(card.contact())
+        #print(card.name_length)
 
 
 #from faker import Faker
