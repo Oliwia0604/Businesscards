06525c2842dbc064c676ff3d81e398dec8df590b
Oliwia <oliwia.wojtaszczyk@gmail.com>
2020-07-29 15:47:19 +0200

    checking

--- businesscard.py
+++ businesscard.py
@@ -52,7 +52,7 @@ def create_contacts(card, quantity):
         print(item.label_lenght, item._str_())
 
 
-if name == "_main_":
+if _name_ == "_main_":
 
     create_contacts(BusinessContact, 10)
     create_contacts(BaseContact, 10)
