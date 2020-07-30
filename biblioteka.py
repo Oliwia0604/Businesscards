53c41f295eef8e1861571b43c7b27bb0e4863b62
Oliwia <oliwia.wojtaszczyk@gmail.com>
2020-07-30 23:47:19 +0200

    check

--- biblioteka.py
+++ biblioteka.py
@@ -1,29 +1,31 @@
 print("Biblioteka filmow")
 
-class Films:
-    def __init__(self, film_title, film_released, film_genre, number_of_plays):
-        self.film_title=film_title
-        self.film_released=film_released
-        self.film_genre=film_genre
-        self.number_of_plays=number_of_plays
+class Movies:
+    def __init__(self, movie_title, movie_released, movie_genre, movie_number_of_plays):
+        self.movie_title=movie_title
+        self.movie_released=movie_released
+        self.movie_genre=movie_genre
+        self.movie_number_of_plays=movie_number_of_plays
 
-a=Films("The Irishman","2019", "dramat", "5000")
-b=Films("Pierwszy czlowiek","2018", "dramat", "300")
-c=Films("Shawshank", "1994", "dramat", "2000")
-d=Films("Forrest Gump", "1994", "dramat", "1500")
-e=Films("Django", "2013", "western", "1000")
+a=Movies("The Irishman","2019", "dramat", "5000")
+b=Movies("Pierwszy czlowiek","2018", "dramat", "300")
+c=Movies("Shawshank", "1994", "dramat", "2000")
+d=Movies("Forrest Gump", "1994", "dramat", "1500")
+e=Movies("Django", "2013", "western", "1000")
 
-Films=[a, b, c, d, e]
+Movies=[a, b, c, d, e]
+
+
 
 
 class series:
-    def __init__(self, series_title, series_released, series_genre, episode_number, season_number, number_of_plays):
+    def __init__(self, series_title, series_released, series_genre, episode_number, season_number, series_number_of_plays):
         self.series_title=series_title
         self.series_released=series_released
         self.series_genre=series_genre
         self.episode_number=episode_number
         self.season_number=season_number
-        self.number_of_plays=number_of_plays
+        self.series_number_of_plays=series_number_of_plays
 
 f=series("Stranger Things","2019", "Horror", "S02", "E04", "123")
 g=series("Dom z papieru", "2020", "Kryminal", "S04", "E03", "52")
@@ -33,15 +35,16 @@ j=series("Breaking Bad", "2013", "Kryminal", "S01", "E04", "48")
 
 series=[f, g, h, i, j]
 
-for films in films:
-    print(f"{film_title} ({film_released})")
+for movie in movies:
+        print(f"{movie_title} ({movie_released})")
+    
 
 for series in series:
-    print(f"{film_series} {episode_number}{season_number}"
+        print(f"{film_series} {episode_number}{season_number}"
 
 
 
-#def play(self): 
-  #  self.number_of_plays += 1
+#def play(movies, series): 
+ #   self.number_of_plays += 1
+    
 
-   
